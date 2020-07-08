8bd2ed32a7526788491a207deadc52b986710f6b
Melissa Castorena <mcastorena0316@gmail.com>
2020-07-08 11:14:54 -0500

    fix grammar mistakes and position of social media links on hero section

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,18 +11,18 @@ export const headData = {
 export const heroData = {
   title: 'Hi, my name is',
   name: 'Melissa Castorena',
-  subtitle: "I'm a Full Stack Developer.",
+  subtitle: "I'm a Full-Stack Developer.",
   cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   paragraphOne:
-    'My name is Melissa and I’m a Mexican full-stack developer who in her past life was an Industrial Engineer. Some of my tech skills are HTML5, CSS3, Databases, Ruby, Ruby On Rails,Javascript, React and Redux. ',
+    'My name is Melissa and I’m a Mexican full-stack developer who in her past life was an Industrial Engineer. Some of my tech skills are HTML5, CSS3, Databases, Ruby, Ruby On Rails, Javascript, React, and Redux.',
   paragraphTwo:
-    ' When I’m not coding, I enjoy: cooking for others (usually my husband), play around with Benito and Lucho (my cats and bosses), and spend more time outdoors as possible.',
+    ' When I’m not coding, I enjoy: cooking for others (usually my husband), playing around with Benito (my cat and boss), and spending more time outdoors as possible',
   paragraphThree:
-    "Below you can see the result of my +1300 hours of work I’ve been doing for the last months. Some of them were made working on pair programming, an amazing experience that helped me interact with people from all over the world. Feel free to give them a look. I'm always available to have a good conversation, so don't be shy to contact me by mail or twitter.",
+    "Below you can see the result of my +1500 hours of work I’ve been doing for the last months. Some of them were made working with remote pair programming, an amazing experience that helped me interact with people from all over the world. Feel free to give them a look. I'm always available to have a good conversation, so don't be shy to contact me by mail or Twitter.",
   resume: 'https://resume.creddle.io/resume/7e3mp9wmmwy',
 };
 
@@ -33,7 +33,7 @@ export const projectsData = [
     img: 'spacex.png',
     title: 'SpaceX Launch Missions',
     info:
-      'The object of this project was to build with React and Redux, a catalog that contains a browsable list of items, these can be filtered and also, can access the details of each one of them.',
+      'The objective of this project was to build with React and Redux, a catalog that contains a browsable list of items, these can be filtered and also, can access the details of each one of them.',
     info2: 'This catalog includes information about the launches executed by the company SpaceX.',
     url: 'https://react-redux-spacex.herokuapp.com/',
     repo: 'https://github.com/mcastorena0316/react-redux-capstone',
@@ -49,7 +49,7 @@ export const projectsData = [
     info:
       'Front-end application built with React & Redux that helps organize and manage a bookstore. ',
     info2:
-      'Each book has its information including title, name, id, category, and % of the books that have been read. The user can filter add and erase a book and also filter them by category,',
+      'Each book has its information including title, name, id, category, and % of the books that have been read. The user can filter add and erase a book and also filter them by category.',
     url: 'https://bookstorecms.herokuapp.com/',
     repo: 'https://github.com/mcastorena0316/Bookstore_CMS',
     tec1: 'React',
@@ -60,9 +60,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'falsebook.png',
     title: 'FACEBOOK-CLONE',
-    info:
-      'In this project, we were requested to build a Facebook-like social network application including some of the core features of this social media: users, profiles, friendships, posts, news-feed, and likes.',
-    info2: '',
+    info: 'In this project, we build a Ruby on Rails app with a similar behavior than Facebook.',
+    info2:
+      'In this app, users can: Sign up with a real Facebook account and with a normal one, add/remove friends, send or accept friend requests, create posts and comments, and like and unlike their own posts/comments and their friends.',
     url: 'https://young-fortress-61632.herokuapp.com/',
     repo: 'https://github.com/mcastorena0316/facebook-clone',
     tec1: 'Ruby on Rails',
@@ -74,9 +74,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'sampleapp2.png',
     title: 'SAMPLE TWITTER',
-    info: 'This project consists of a sample twitter-like applicatioN',
+    info: 'This project consists of a sample twitter-like application.',
     info2:
-      'It includes the next features: users sign in with authentication, log in with validation, profiles, followers and following, micro-posts with images, among others.',
+      'It includes the next features: users sign in with authentication, log in with validation, profiles, followers, and following, micro-posts with images, among others.',
     url: 'https://pacific-plateau-91098.herokuapp.com/',
     repo: 'https://github.com/mcastorena0316/sample_app',
     tec1: 'Ruby on Rails',
@@ -88,7 +88,7 @@ export const projectsData = [
     img: 'game2.png',
     title: 'Shooter Game: CatGun',
     info:
-      'The objective of this project was to design and build a platform game like the one in Space Shooter Game using Phaser and Javascript among other technologies.',
+      'The objective of this project was to design and build a platform game like Space- Shooter using Phaser and Javascript among other technologies.',
     info2:
       "This game is inspired in my cat 'Benito' and the villains include one of his worst enemies: dogs, snakes (and reptiles in general), and the representation of my husband who is always trying to hug him (which he doesn't like at all).",
     url: 'https://cat-gun.netlify.app/',
@@ -103,9 +103,9 @@ export const projectsData = [
     img: 'illnest2.png',
     title: 'illnest',
     info:
-      'The objective of this project was to create a mobile tracking App in which a user is presented with a list of things to be measured. For the Front End, I used React and Ruby on Rails for the Back-End API',
+      'The objective of this project was to create a mobile tracking App in which a user is presented with a list of things to be measured. I made the front-end with React and Redux. And for the backend API, I used Ruby on Rails.',
     info2:
-      'On illnest, you can track a specific illness you have, whether is chronic or is a recent affliction, it can be measure here.',
+      'At illnest, the user can monitor daily for a specific disease that they have, whether chronic or a recent condition, can be measured here. Some of the data you can track daily includes the mood of the day, temperature, medicines consumed, and the symptoms the user presented on that day.',
     url: 'https://illnest.netlify.app/',
     repo: 'https://github.com/mcastorena0316/final-capstone',
     tec1: 'React-Redux',
